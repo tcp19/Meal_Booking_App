e692b46312af83d6d8a1d79649a507fb81eae224
mohammed <ibrahimdamy7@gmail.com>
2019-12-11 20:52:39 +0100

    ft(file structure): format files

--- src/server.js
+++ src/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+// formated response
+import jsonResponse from './helpers/jsonResponse';
+
 
 // Configure .env
 dotenv.config();
 const app = express();
 
+// configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 const PORT = process.env.PORT || 5000;
 
 
 app.get('/', (req, res) => {
-  res.send('hello');
+  return jsonResponse.success(res, 'success', 200, 'welcome to the Meal App api');
 })
 
 app.listen(PORT, () => {
