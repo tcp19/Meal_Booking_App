6eaf7c8e8071580a178fda0b82ba00a05ff1a216
mohammed <ibrahimdamy7@gmail.com>
2019-12-16 02:47:46 +0100

    chore(dotenv):  use environment variable

--- src/models/db.js
+++ src/models/db.js
@@ -1,16 +1,14 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connection = {
-    user: 'postgres',
-    password: 'root',
-    server: 'localhost',
-    port: '5432',
-    database: 'mealapp'
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // server: process.env.DB_SERVER,
-    // port: process.env.DB_PORT,
-    // database: process.env.DB_DATABASE
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 }
 
 const pool = new pg.Pool(connection)
