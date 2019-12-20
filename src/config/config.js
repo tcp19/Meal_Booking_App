b96030892ba217c0e6ff107d5cccf75d5236b3fa
Mr-emeka <speak2c.emeka@gmail.com>
2019-12-20 22:18:39 +0100

    bug(added escape): added escape to special characters on travis

--- src/config/config.js
+++ src/config/config.js
@@ -12,7 +12,11 @@ module.exports = {
     database: process.env.DEV_DATABASE
   },
   test: {
-   use_env_variable: process.env.TEST_DB_URL
+    user: process.env.TEST_USER,
+    password: process.env.TEST_PASSWORD,
+    server: process.env.TEST_SERVER,
+    port: process.env.TEST_PORT,
+    database: process.env.TEST_DATABASE
   },
   production: {
     user: process.env.DB_USER,
